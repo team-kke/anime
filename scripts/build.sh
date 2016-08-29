7f514619a51e529c7961231813b90f7501fab219
Jun <me@noraesae.net>
2016-08-29 23:38:15 +0900

    Set git config for build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+git config --global user.email "me@noraesae.net"
+git config --global user.name "Anime Missionary"
+
 npm run build
 if git commit . -m "[CircleCI] Build"; then
   git push origin master
