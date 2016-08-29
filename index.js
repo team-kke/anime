0f0cbac096af122d0319c1c1401051804fd3e2d1
Jun <me@noraesae.net>
2016-08-29 23:20:36 +0900

    Finish build system

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const ejs = require('ejs');
 const fs = require('fs');
 
-const list = [
-  { title: '어떤 마술의 금서목록 1화', date: '2016-08-29', quote: { jp: 'よし！グランドシーズン！', kr: '요시! 그란도시즌!' } },
-];
+const list = fs.readdirSync(`${__dirname}/lists`)
+  .sort()
+  .reverse()
+  .map(file => {
+    const lines = fs.readFileSync(`${__dirname}/lists/${file}`).toString().split('\n');
+    return {
+      title: lines[0],
+      date: file,
+      quote: {
+        jp: lines[1],
+        kr: lines[2],
+      },
+    };
+  });
 
-const template = fs.readFileSync(__dirname + '/templates/index.ejs').toString();
+const template = fs.readFileSync(`${__dirname}/templates/index.ejs`).toString();
 console.log(ejs.render(template, { list }));
